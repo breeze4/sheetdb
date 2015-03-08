22eb8c5fd029d29350d0c020278ff64921287bec
breeze4 <ambiturner0@gmail.com>
2015-03-07 16:43:18 -0800

    refined the result a little

--- src/sheetdb/core.clj
+++ src/sheetdb/core.clj
@@ -10,14 +10,15 @@
 (defn- url [key]
   (str "https://spreadsheets.google.com/feeds/list/" key "/od6/public/values?alt=json"))
 
-(feed/parse-feed "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values?alt\\u003djson")
+; atom feed as a map - use this for configuration later
+;(feed/parse-feed "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values?alt\\u003djson")
 
 (defn- get-data
   "[sheet-key] Gets the entire spreadsheet with the given key"
   [sheet-key]
   (client/get (url sheet-key)))
 
-(def data (json/read-str ((get-data "1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w") :body)))
+(def data (json/read-str ((get-data sheet-key) :body)))
 
 ; list of the entries
 ; [{"gsx$name" {"$t" "sarah conner"},
@@ -28,7 +29,6 @@
 ;   "gsx$rank" {"$t" "robot killer"},
 ;   "id" {"$t" "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cpzh4"},
 ;   "gsx$id" {"$t" "2"}}]
-(def entries ((data "feed") "entry"))
 ; want:
 ;{"https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cokwr"
 ; {"gsx$name" {"$t" "sarah conner"},
@@ -40,6 +40,7 @@
 ;  "gsx$rank" {"$t" "robot killer"},
 ;  "id" {"$t" "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cpzh4"},
 ;  "gsx$id" {"$t" "2"}}}
+(def entries ((data "feed") "entry"))
 
 ; this gets the set of all row names - is that correct? maybe correct and not efficient
 ; an alternative way and much simpler and predictably incorrect is to use the first entry in the list
@@ -50,15 +51,21 @@
   (keys (first row)))
 
 (def gsx-prefix "gsx$")
-(defn- user-col? [col-name]
-  (if (.contains col-name gsx-prefix)
-    ; return the name of the column or nil if not contained
-    ; consumer can do a when-let, I think? if-let?
-    true
-    false))
+; TODO: make which field to use as an id configurable
+;(def default-id-field "id")
+(def default-id-field (str gsx-prefix "id"))
+(def gsx-prefix-re #"(gsx\$)(.+)")
 
-(defn- get-id [ent]
-  ((ent "id") "$t"))
+(defn- strip-prefix [str]
+  (if-let [match (re-find gsx-prefix-re str)]
+    (match 2)
+    str))
+
+(defn- user-col? [col-name]
+  (.startsWith col-name gsx-prefix))
+
+(defn- get-id [key ent]
+  ((ent key) "$t"))
 
 (defn- user-col-keys
   "returns set of column names as strings"
@@ -70,16 +77,19 @@
 
 (defn convert-row [ent]
   (let [; get the url as an id; could construct a unique ID but this is fine for now
-        key (get-id ent)
+        key (get-id default-id-field ent)
         ; get a lazy sequence of the user columns, ignore the other columns
         cols (seq (user-col-keys ent))
+        ; formatted column names
+        stripped-col-names (map #(strip-prefix %) cols)
         ; filtering which values to pluck out of the sheet by which values are user entered
-        vs (map #(ent %) cols)]
+        vs (map #(ent %) cols)
+        ; flatten down and just get the values, not the "$t" word
+        stripped-vals (flatten (map #(vals %) vs))]
     ; create a map of the key to the user values
-    (assoc {} key (zipmap cols vs))))
+    (assoc {} key (zipmap stripped-col-names stripped-vals))))
 
 (defn convert-rows [ents]
   (into {} (map #(convert-row %) ents)))
-
 
 ;;
--- test/sheetdb/core_test.clj
+++ test/sheetdb/core_test.clj
@@ -2,6 +2,28 @@
   (:require [clojure.test :refer :all]
             [sheetdb.core :refer :all]))
 
+; list of the entries
+; [{"gsx$name" {"$t" "sarah conner"},
+;  "gsx$rank" {"$t" "chief survivor"},
+;  "id" {"$t" "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cokwr"},
+;  "gsx$id" {"$t" "1"}},
+;  {"gsx$name" {"$t" "john conner"},
+;   "gsx$rank" {"$t" "robot killer"},
+;   "id" {"$t" "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cpzh4"},
+;   "gsx$id" {"$t" "2"}}]
+; want:
+;{"https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cokwr"
+; {"gsx$name" {"$t" "sarah conner"},
+;  "gsx$rank" {"$t" "chief survivor"},
+;  "id" {"$t" "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cokwr"},
+;  "gsx$id" {"$t" "1"}}
+; "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cpzh4"
+; {"gsx$name" {"$t" "john conner"},
+;  "gsx$rank" {"$t" "robot killer"},
+;  "id" {"$t" "https://spreadsheets.google.com/feeds/list/1vAb70Ti_hMVyxlxgnMNsj5YQhTy4S93L2wscFslmE5w/od6/public/values/cpzh4"},
+;  "gsx$id" {"$t" "2"}}}
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
